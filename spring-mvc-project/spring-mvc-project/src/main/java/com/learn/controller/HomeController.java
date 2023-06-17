5a5be52119e70fc4c76af80bb8a4b0ebbffe88da
Rushikesh Malvadkar <rushikesh@dev.com>
2023-06-17 17:36:27 +0530

    Implented Spring MVC

---
+++ spring-mvc-project/spring-mvc-project/src/main/java/com/learn/controller/HomeController.java
@@ -0,0 +1,26 @@
+package com.learn.controller;
+
+import com.learn.entity.ProductEntity;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import javax.persistence.EntityManager;
+import javax.persistence.PersistenceContext;
+
+@Controller
+public class HomeController {
+
+    private Logger log = LoggerFactory.getLogger(HomeController.class);
+
+    @RequestMapping("/status")
+    public String status(){
+        log.info("<<<<<<<< start() Changed");
+        log.info("start() >>>>>>");
+        return "status";
+    }
+
+}
---
+++ spring-mvc-project/spring-mvc-project/src/main/webapp/WEB-INF/views/status.jsp
@@ -0,0 +1,5 @@
+<html>
+<body>
+<h2>Your application is UP</h2>
+</body>
+</html>

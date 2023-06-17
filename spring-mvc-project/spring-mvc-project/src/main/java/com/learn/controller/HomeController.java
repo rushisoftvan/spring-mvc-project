10985a5b101cea43c7d73b31c3b73d58bf91e5fb
Rushikesh Malvadkar <rushikesh@dev.com>
2023-06-17 17:37:42 +0530

    Removed unnecesary imports

--- spring-mvc-project/spring-mvc-project/src/main/java/com/learn/controller/HomeController.java
+++ spring-mvc-project/spring-mvc-project/src/main/java/com/learn/controller/HomeController.java
@@ -1,6 +1,5 @@
 package com.learn.controller;
 
-import com.learn.entity.ProductEntity;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
